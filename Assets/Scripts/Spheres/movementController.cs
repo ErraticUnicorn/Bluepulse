e8872abfe96edf39291eb9b4a15705ec13909dc9
David Wert <djw2yw@virginia.edu>
2014-09-05 15:50:25 -0400

    adds basic spawner (limited to 5 for now)

--- Assets/Scripts/Spheres/movementController.cs
+++ Assets/Scripts/Spheres/movementController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class movementController : MonoBehaviour {
 
-    public Vector2 speed = new Vector2(5, 5);
+    public Vector2 speed = new Vector2(10, 10);
     public Vector2 direction = new Vector2 (1, 1);
 
     private Vector2 movement;
@@ -19,20 +19,11 @@ public class movementController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        leftEdge = Camera.main.ViewportToWorldPoint (new Vector3(0.0f,0.0f, 0.0f));
-        rightEdge = Camera.main.ViewportToWorldPoint ( new Vector3(1.0f,0.0f, 0.0f));
-        bottomEdge = Camera.main.ViewportToWorldPoint ( new Vector3(0.0f,0.0f, 0.0f));
-        topEdge = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, 1.0f, 0.0f));
+        leftEdge = Camera.main.ViewportToWorldPoint (new Vector3(-.25f,0.0f, 0.0f));
+        rightEdge = Camera.main.ViewportToWorldPoint ( new Vector3(1.25f,0.0f, 0.0f));
+        bottomEdge = Camera.main.ViewportToWorldPoint ( new Vector3(0.0f,-.25f, 0.0f));
+        topEdge = Camera.main.ViewportToWorldPoint(new Vector3(0.0f, 1.25f, 0.0f));
 
-        var randomNumber = Random.Range(0, 2);
-        if (randomNumber <= .5f)
-        {
-            rigidbody2D.AddForce(speed);
-        }
-        else
-        {
-            rigidbody2D.AddForce(-speed);
-        }
 	}
 
     void FixedUpdate() {
@@ -40,25 +31,21 @@ public class movementController : MonoBehaviour {
 
         if (transform.position.x > rightEdge.x)
         {
-            Debug.Log("Off the screen");
             curSpeed.x = -this.rigidbody2D.velocity.x;
             this.rigidbody2D.velocity = curSpeed;
         }
         if (transform.position.x < leftEdge.x)
         {
-            Debug.Log("Off the screen");
             curSpeed.x = -this.rigidbody2D.velocity.x;
             this.rigidbody2D.velocity = curSpeed;
         }
         if (transform.position.y < bottomEdge.y)
         {
-            Debug.Log("off the bottom");
             curSpeed.y = -this.rigidbody2D.velocity.y;
             this.rigidbody2D.velocity = curSpeed;
         }
         if (transform.position.y > topEdge.y)
         {
-            Debug.Log("off the top");
             curSpeed.y = -this.rigidbody2D.velocity.y;
             this.rigidbody2D.velocity = curSpeed;
         }
---
+++ Assets/Scripts/sphereSpawner.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class sphereSpawner : MonoBehaviour {
+
+    public GameObject sphere;
+    public int spherePoolCount = 20;
+    private GameObject[] spheres;
+    private List<GameObject> spawnedSpheres;
+
+    Vector3 leftSpawnpoint;
+    Vector3 rightSpawnpoint;
+
+
+	// Use this for initialization
+	void Start () {
+        spawnedSpheres = new List<GameObject>();
+
+	}
+
+    void Awake() {
+        spheres = new GameObject[spherePoolCount];
+        for (int i = 0; i < spheres.Length; i++) {
+            spheres[i] = Instantiate(sphere) as GameObject;
+            spheres[i].SetActive(false);
+            spheres[i].transform.parent = this.transform.parent;
+        }
+        Vector3 leftSpawnpoint = Camera.main.ViewportToWorldPoint(new Vector3(-.20f, 0.5f, 0.0f));
+        Vector3 rightSpawnpoint = Camera.main.ViewportToWorldPoint(new Vector3(1.20f, 0.5f, 0.0f));
+
+        for (int i = 0; i < 5; i++) {
+            movementController mvmt = spheres[i].GetComponent<movementController>();
+            spheres[i].SetActive(true);
+            Vector2 direction;
+            direction.y = Random.Range(-.75f, .75f);
+            float leftOrRight = Random.Range(0f, 1f);
+            if(leftOrRight < .5){
+                spheres[i].transform.position = new Vector3(leftSpawnpoint.x, leftSpawnpoint.y, this.transform.position.z);
+                direction.x = Random.Range(0f, 1f);
+                mvmt.addForce(1, direction);
+            } else {
+                spheres[i].transform.position = new Vector3(rightSpawnpoint.x, rightSpawnpoint.y, this.transform.position.z);
+                direction.x = Random.Range(-1f, 0f);
+                mvmt.addForce(1, direction);
+            }
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    
+	}
+}
