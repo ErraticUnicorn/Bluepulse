dbeff1b2c1f13aa4f9c43a6fc95538a5b40aeffa
AgentE382 <AgentE382@gmail.com>
2014-12-06 13:24:30 -0500

    Adds hold mechanic

--- Assets/Scripts/Spheres/inputController.cs
+++ Assets/Scripts/Spheres/inputController.cs
@@ -8,6 +8,8 @@ public class inputController : MonoBehaviour {
     private Vector3 mousePos;
     private colorController currentColor;
     private movementController movement;
+	public float heldPercentage;
+	public float maxHoldTime;
 
 	// Use this for initialization
 	void Start () {
@@ -18,7 +20,10 @@ public class inputController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+		if (actionOngoing && heldPercentage <= 1.0f)
+		{
+			heldPercentage = Mathf.Min(heldPercentage + Time.deltaTime / maxHoldTime, 1.0f);
+		}
 	}
 
     void OnMouseDown() {
@@ -27,6 +32,7 @@ public class inputController : MonoBehaviour {
             lastPos = this.transform.position;
             movement.stopMovement();
             actionOngoing = true;
+			heldPercentage = 0.0f;
         }
     }
 
@@ -38,7 +44,7 @@ public class inputController : MonoBehaviour {
             Vector3 heading = mousePos - lastPos;
             float distance = heading.magnitude;
             Vector3 direction = heading / distance;
-            movement.addForce(distance, direction);
+            movement.addForce(distance * heldPercentage, direction);
         }
         actionOngoing = false;
     }
